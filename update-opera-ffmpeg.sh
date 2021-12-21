6aa1892235c64624ea3a8dd49f6ad51a1582a368
Rémi AUGUSTE <remi.auguste@gmail.com>
2021-12-21 11:22:30 +0100

    now using linux mint repo to get updated lib ffmpeg for opera

--- update-opera-ffmpeg.sh
+++ update-opera-ffmpeg.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /tmp
-wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/$(curl -s http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/ |grep chromium-codecs-ffmpeg-extra.*0ubuntu0\.18.*_amd64.deb |tail -1 |sed -n "/href=/s/.*href=\([^>]*\).*/\1/p" |xargs)
-dpkg -x chromium-codecs-ffmpeg-extra*.deb /tmp/chromium
-sudo cp /tmp/chromium/usr/lib/chromium-browser/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
+PKG_REPO="https://ftp5.gwdg.de/pub/linux/debian/mint/packages/pool/upstream/c/chromium/"
+
+version=$(opera --version | cut -d'.' -f1)
+((version+=14))
+
+wget $PKG_REPO$(curl -s $PKG_REPO | grep "amd64" | grep "chromium_$version" | tail -1 | sed -n "/href=/s/.*href=\([^>]*\).*/\1/p" | xargs)
+dpkg -x chromium*.deb /tmp/chromium
+sudo cp /tmp/chromium/usr/lib/chromium/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
 rm -rf /tmp/chromium /tmp/chromium*.deb
