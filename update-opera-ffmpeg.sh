1d770ad94d7da5602c07f8c5c409f25be2edb428
Rémi AUGUSTE <remi.auguste@gmail.com>
2022-01-05 12:51:19 +0100

    now detecting opera's path and using the lib_extra folder

--- update-opera-ffmpeg.sh
+++ update-opera-ffmpeg.sh
@@ -7,5 +7,7 @@ version=$(opera --version | cut -d'.' -f1)
 
 wget $PKG_REPO$(curl -s $PKG_REPO | grep "amd64" | grep "chromium_$version" | tail -1 | sed -n "/href=/s/.*href=\([^>]*\).*/\1/p" | xargs)
 dpkg -x chromium*.deb /tmp/chromium
-sudo cp /tmp/chromium/usr/lib/chromium/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
+opera_path=$(dirname $(readlink $(which opera)))
+sudo mkdir -p $opera_path/lib_extra/
+sudo cp /tmp/chromium/usr/lib/chromium/libffmpeg.so $opera_path/lib_extra/
 rm -rf /tmp/chromium /tmp/chromium*.deb
